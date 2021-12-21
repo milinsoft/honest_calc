f942006c3a8513281b032c4abb6ae047846dcee5
parseltongist <>
2021-12-21 12:46:11 +0100

    Stage 5/5 final version

--- honest_calc.py
+++ honest_calc.py
@@ -45,7 +45,7 @@ class HonestCalculator:
         print(self.messages[4])
         answer_store = input().lower()
         if answer_store == "y":
-            if not is_one_digit(self.result):
+            if not HonestCalculator.is_one_digit(self.result):
                 HonestCalculator.memory = self.result
             else:
                 def obtain_answer(msg_index):
@@ -80,57 +80,57 @@ class HonestCalculator:
         else:
             return self.continue_calc()
 
+    @staticmethod
+    def is_one_digit(digit) -> bool:
+        return all([(-10 < digit < 10),  isinstance(digit, float), digit % 1 == 0])  # last statement needed purely for lazy messages
 
-def is_one_digit(digit) -> bool:
-    return all([(-10 < digit < 10),  isinstance(digit, float), digit % 1 == 0])  # last statement needed purely for lazy messages
+    @staticmethod
+    def check(v1, v2, v3):
+        msg = ""
+        if all([HonestCalculator.is_one_digit(v1), HonestCalculator.is_one_digit(v2)]):
+            msg += HonestCalculator.messages[6]
+        if all([v3 == "*", any([v2 == 1, v1 == 1])]):
+            msg += HonestCalculator.messages[7]
+        if any([(v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-")]):
+            msg += HonestCalculator.messages[8]
+        if msg != "":
+            msg = HonestCalculator.messages[9] + msg
+            print(msg)
 
+    def run_calculator(self):
+        if self.x == "M":
+            self.x = HonestCalculator.memory
+        if self.y == "M":
+            self.y = HonestCalculator.memory
 
-def check(v1, v2, v3):
-    msg = ""
-    if all([is_one_digit(v1), is_one_digit(v2)]):
-        msg = msg + HonestCalculator.messages[6]
-    if all([v3 == "*", any([v2 == 1, v1 == 1])]):
-        msg = msg + HonestCalculator.messages[7]
-    if any([(v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-")]):
-        msg = msg + HonestCalculator.messages[8]
-    if msg != "":
-        msg = HonestCalculator.messages[9] + msg
-        print(msg)
+        if any([HonestCalculator.is_invalid_type(self.x), HonestCalculator.is_invalid_type(self.y)]):
+            print(self.messages[1])
+            return main()
+        else:
+            self.x, self.y = float(self.x), float(self.y)
+            if self.oper in self.ops:
+
+                HonestCalculator.check(self.x, self.y, self.oper)
+                try:
+                    self.result = self.ops[self.oper](self.x, self.y)
+                except ZeroDivisionError:
+                    print(self.messages[3])
+                    return main()
+
+                print(self.result)
+
+                self.result_saver()
+
+                self.continue_calc()
+            else:
+                print(self.messages[2])
+                return main()
 
 
 def main():
     print(HonestCalculator.messages[0])
-    # read calc
-    calc = HonestCalculator.from_string(input())
-
-    if calc.x == "M":
-        calc.x = HonestCalculator.memory
-    if calc.y == "M":
-        calc.y = HonestCalculator.memory
-
-    if any([HonestCalculator.is_invalid_type(calc.x), HonestCalculator.is_invalid_type(calc.y)]):
-        print(calc.messages[1])
-        return main()
-    else:
-        calc.x, calc.y = float(calc.x), float(calc.y)
-        if calc.oper in calc.ops:
-
-            check(calc.x, calc.y, calc.oper)
-
-            try:
-                calc.result = calc.ops[calc.oper](calc.x, calc.y)
-            except ZeroDivisionError:
-                print(calc.messages[3])
-                return main()
-
-            print(calc.result)
-
-            calc.result_saver()
-
-            calc.continue_calc()
-        else:
-            print(calc.messages[2])
-            return main()
+    calculator = HonestCalculator.from_string(input())
+    calculator.run_calculator()
 
 
 if __name__ == '__main__':
